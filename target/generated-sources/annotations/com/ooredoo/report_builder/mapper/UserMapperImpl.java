604064a4e17c6aae12492c0ceea65489362d6064
aliobba <obbaali@gmail.com>
2025-10-05 14:50:35 +0200

    deploy: refact arch project to deploy it in render

---
+++ target/generated-sources/annotations/com/ooredoo/report_builder/mapper/UserMapperImpl.java
@@ -0,0 +1,33 @@
+package com.ooredoo.report_builder.mapper;
+
+import com.ooredoo.report_builder.dto.response.UserResponseDTO;
+import com.ooredoo.report_builder.user.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-05T14:08:45+0200",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.43.0.v20250819-1513, environment: Java 21.0.8 (Eclipse Adoptium)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserResponseDTO toDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponseDTO.UserResponseDTOBuilder userResponseDTO = UserResponseDTO.builder();
+
+        userResponseDTO.accountLocked( user.isAccountLocked() );
+        userResponseDTO.email( user.getEmail() );
+        userResponseDTO.enabled( user.isEnabled() );
+        userResponseDTO.firstname( user.getFirstname() );
+        userResponseDTO.id( user.getId() );
+        userResponseDTO.lastname( user.getLastname() );
+
+        return userResponseDTO.build();
+    }
+}
---
+++ target/generated-sources/annotations/com/ooredoo/report_builder/mapper/ZoneMapperImpl.java
@@ -0,0 +1,13 @@
+package com.ooredoo.report_builder.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-05T14:08:45+0200",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.43.0.v20250819-1513, environment: Java 21.0.8 (Eclipse Adoptium)"
+)
+@Component
+public class ZoneMapperImpl implements ZoneMapper {
+}
